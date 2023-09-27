00336f461438e137b6a6a933997104152d132b2e
izabellllia <elizavetazhitnik@MacBook-Air-Elizaveta-3.local>
2023-09-27 22:16:33 +0700

    at2

--- Task_1_1_2/src/test/java/zhitnik/MainTest.java
+++ Task_1_1_2/src/test/java/zhitnik/MainTest.java
@@ -2,69 +2,9 @@ package zhitnik;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**Тест.*/
 public class MainTest {
-    /**plus1.*/
-    @Test
-    public void testPlus() {
-        int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
-        int[] coefficients2 = {4, 5, 6}; // 4x^2 + 5x + 6
-
-        Main polynomial1 = new Main(coefficients1);
-        Main polynomial2 = new Main(coefficients2);
-        Main sum = polynomial1.plus(polynomial2);
-
-        int[] expectedCoefficients = {5, 7, 9};
-
-        assertArrayEquals(expectedCoefficients, sum.getCoefficients());
-    }
-
-    /**plus2.*/
-    @Test
-    public void testPlus2() {
-        int[] coefficients1 = {0, 0, 0, 5};
-        int[] coefficients2 = {3, -2, 1};
-
-        Main polynomial1 = new Main(coefficients1);
-        Main polynomial2 = new Main(coefficients2);
-        Main sum = polynomial1.plus(polynomial2);
-
-        int[] expectedCoefficients = {3, -2, 1, 5};
-
-        assertArrayEquals(expectedCoefficients, sum.getCoefficients());
-    }
-
-    /**minus.*/
-    @Test
-    public void testMinus() {
-        int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
-        int[] coefficients2 = {4, 5, 6}; // 4x^2 + 5x + 6
-
-        Main polynomial1 = new Main(coefficients1);
-        Main polynomial2 = new Main(coefficients2);
-        Main difference = polynomial1.minus(polynomial2);
-
-        int[] expectedCoefficients = {-3, -3, -3};
-
-        assertArrayEquals(expectedCoefficients, difference.getCoefficients());
-    }
-
-    /**times.*/
-    @Test
-    public void testTimes() {
-        int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
-        int[] coefficients2 = {4, 5}; // 4x + 5
-
-        Main polynomial1 = new Main(coefficients1);
-        Main polynomial2 = new Main(coefficients2);
-        Main product = polynomial1.times(polynomial2);
-
-        int[] expectedCoefficients = {4, 13, 22, 15};
-
-        assertArrayEquals(expectedCoefficients, product.getCoefficients());
-    }
 
     @Test
     void testMainRunning() {
