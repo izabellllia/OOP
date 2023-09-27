46fe5234db8b7884edc2a46e685124e9c112e986
izabellllia <elizavetazhitnik@MacBook-Air-Elizaveta-3.local>
2023-09-27 19:36:02 +0700

    new6

--- Task_1_1_2/src/test/java/zhitnik/MainTest.java
+++ Task_1_1_2/src/test/java/zhitnik/MainTest.java
@@ -49,4 +49,47 @@ public class MainTest {
 
         assertArrayEquals(expectedCoefficients, product.getCoefficients());
     }
+
+    @Test
+    public void testPlus2() {
+        int[] coefficients1 = {2, 3, 4}; // 2x^2 + 3x + 4
+        int[] coefficients2 = {5, 6, 7}; // 5x^2 + 6x + 7
+
+        Main polynomial1 = new Main(coefficients1);
+        Main polynomial2 = new Main(coefficients2);
+        Main sum = polynomial1.plus(polynomial2);
+
+        int[] expectedCoefficients = {7, 9, 11};
+
+        assertArrayEquals(expectedCoefficients, sum.getCoefficients());
+    }
+
+    @Test
+    public void testMinus2() {
+        int[] coefficients1 = {2, 3, 4}; // 2x^2 + 3x + 4
+        int[] coefficients2 = {5, 6, 7}; // 5x^2 + 6x + 7
+
+        Main polynomial1 = new Main(coefficients1);
+        Main polynomial2 = new Main(coefficients2);
+        Main difference = polynomial1.minus(polynomial2);
+
+        int[] expectedCoefficients = {-3, -3, -3};
+
+        assertArrayEquals(expectedCoefficients, difference.getCoefficients());
+    }
+
+    @Test
+    public void testTimes2() {
+        int[] coefficients1 = {2, 3, 4}; // 2x^2 + 3x + 4
+        int[] coefficients2 = {5, 6}; // 5x + 6
+
+        Main polynomial1 = new Main(coefficients1);
+        Main polynomial2 = new Main(coefficients2);
+        Main product = polynomial1.times(polynomial2);
+
+        int[] expectedCoefficients = {10, 27, 38, 24};
+
+        assertArrayEquals(expectedCoefficients, product.getCoefficients());
+    }
+
 }
