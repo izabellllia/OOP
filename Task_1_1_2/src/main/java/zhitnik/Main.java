1bf9928f7dd8ba85b6e6d07de0bc0b0707409d25
izabellllia <elizavetazhitnik@MacBook-Air-Elizaveta-3.local>
2023-09-27 17:42:08 +0700

    new5

--- Task_1_1_2/src/main/java/zhitnik/Main.java
+++ Task_1_1_2/src/main/java/zhitnik/Main.java
@@ -9,7 +9,7 @@ public class Main{
     public Main(int[] coefficients) {
         this.degree = coefficients.length - 1;
         this.coefficients = new int[coefficients.length];
-        for (int i = 0; i < coefficients.length; i++) {
+        for (int i = 0; i < coefficients.length; i++){
             this.coefficients[i] = coefficients[i];
         }
     }
@@ -34,18 +34,21 @@ public class Main{
     public Main minus(Main other) {
         int maxDegree = Math.max(this.degree, other.degree);
         int[] resultCoefficients = new int[maxDegree + 1];
+
         for (int i = 0; i <= this.degree; i++) {
             resultCoefficients[i] += this.coefficients[i];
         }
+
         for (int i = 0; i <= other.degree; i++) {
             resultCoefficients[i] -= other.coefficients[i];
         }
+
         return new Main(resultCoefficients);
     }
 
     /**Таким образом, метод times выполняет умножение
      * двух полиномов, создает новый полином с
-     * полученными коэффициентами и возвращает его*/
+     * полученными коэффициентами и возвращает его.*/
     public Main times(Main other) {
         int resultDegree = this.degree + other.degree;
         int[] resultCoefficients = new int[resultDegree + 1];
@@ -57,6 +60,7 @@ public class Main{
         }
         return new Main(resultCoefficients);
     }
+
     /**Mетод evaluate используется для вычисления значения полинома для заданного значения x.*/
     public double evaluate(double x) {
         double result = 0;
@@ -70,6 +74,7 @@ public class Main{
      * сколько раз нужно продифференцировать объект типа Main.*/
     public Main differentiate(int times) {
         Main result = this;
+
         for (int i = 0; i < times; i++) {
             int[] resultCoefficients = new int[result.degree];
             for (int j = 0; j < result.degree; j++) {
@@ -91,20 +96,24 @@ public class Main{
             }
         }
         return true;
+
     } /**Если ни одно из условий выше не выполняется, то метод возвращает true,
      что означает, что объекты равны.*/
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+
         for (int i = this.degree; i >= 0; i--) {
             if (this.coefficients[i] != 0) {
+
                 if (i != this.degree) {
                     sb.append(" + ");
                 }
                 sb.append(this.coefficients[i]);
+
                 if (i > 0) {
                     sb.append("x");
+
                     if (i > 1) {
                         sb.append("^").append(i);
                     }
@@ -113,7 +122,8 @@ public class Main{
         }
         return sb.toString();
     }
-    /**main*/
+
+    /**main.*/
     public static void main(String[] args) {
         Main p1 = new Main(new int[] {4, 3, 6, 7});
         Main p2 = new Main(new int[] {3, 2, 8});
@@ -135,6 +145,7 @@ public class Main{
             result += coefficients[i] * Math.pow(x, power);
             power++;
         }
+
         return result;
     }
 }
--- Task_1_1_2/src/test/java/zhitnik/MainTest.java
+++ Task_1_1_2/src/test/java/zhitnik/MainTest.java
@@ -1,6 +1,7 @@
 package zhitnik;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.junit.jupiter.api.Test;
 
 /**Тест.*/
@@ -9,10 +10,13 @@ public class MainTest {
     public void testPlus() {
         int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
         int[] coefficients2 = {4, 5, 6}; // 4x^2 + 5x + 6
+
         Main polynomial1 = new Main(coefficients1);
         Main polynomial2 = new Main(coefficients2);
         Main sum = polynomial1.plus(polynomial2);
+
         int[] expectedCoefficients = {5, 7, 9};
+
         assertArrayEquals(expectedCoefficients, sum.getCoefficients());
     }
 
@@ -21,10 +25,13 @@ public class MainTest {
     public void testMinus() {
         int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
         int[] coefficients2 = {4, 5, 6}; // 4x^2 + 5x + 6
+
         Main polynomial1 = new Main(coefficients1);
         Main polynomial2 = new Main(coefficients2);
         Main difference = polynomial1.minus(polynomial2);
+
         int[] expectedCoefficients = {-3, -3, -3};
+
         assertArrayEquals(expectedCoefficients, difference.getCoefficients());
     }
 
@@ -33,10 +40,13 @@ public class MainTest {
     public void testTimes() {
         int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
         int[] coefficients2 = {4, 5}; // 4x + 5
+
         Main polynomial1 = new Main(coefficients1);
         Main polynomial2 = new Main(coefficients2);
         Main product = polynomial1.times(polynomial2);
+
         int[] expectedCoefficients = {4, 13, 22, 15};
+
         assertArrayEquals(expectedCoefficients, product.getCoefficients());
     }
 }
