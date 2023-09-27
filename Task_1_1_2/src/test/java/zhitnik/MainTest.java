79568043a3b6dac7ca71747ca86fd4cdb906d971
izabellllia <elizavetazhitnik@MacBook-Air-Elizaveta-3.local>
2023-09-27 17:28:23 +0700

    new4

--- Task_1_1_2/src/test/java/zhitnik/MainTest.java
+++ Task_1_1_2/src/test/java/zhitnik/MainTest.java
@@ -1,22 +1,18 @@
 package zhitnik;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 import org.junit.jupiter.api.Test;
+
 /**Тест.*/
 public class MainTest {
     @Test
     public void testPlus() {
         int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
         int[] coefficients2 = {4, 5, 6}; // 4x^2 + 5x + 6
-
         Main polynomial1 = new Main(coefficients1);
         Main polynomial2 = new Main(coefficients2);
-
         Main sum = polynomial1.plus(polynomial2);
-
         int[] expectedCoefficients = {5, 7, 9};
-
         assertArrayEquals(expectedCoefficients, sum.getCoefficients());
     }
 
@@ -25,14 +21,10 @@ public class MainTest {
     public void testMinus() {
         int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
         int[] coefficients2 = {4, 5, 6}; // 4x^2 + 5x + 6
-
         Main polynomial1 = new Main(coefficients1);
         Main polynomial2 = new Main(coefficients2);
-
         Main difference = polynomial1.minus(polynomial2);
-
         int[] expectedCoefficients = {-3, -3, -3};
-
         assertArrayEquals(expectedCoefficients, difference.getCoefficients());
     }
 
@@ -41,14 +33,10 @@ public class MainTest {
     public void testTimes() {
         int[] coefficients1 = {1, 2, 3}; // x^2 + 2x + 3
         int[] coefficients2 = {4, 5}; // 4x + 5
-
         Main polynomial1 = new Main(coefficients1);
         Main polynomial2 = new Main(coefficients2);
-
         Main product = polynomial1.times(polynomial2);
-
         int[] expectedCoefficients = {4, 13, 22, 15};
-
         assertArrayEquals(expectedCoefficients, product.getCoefficients());
     }
 }
