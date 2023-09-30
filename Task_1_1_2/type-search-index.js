6fad209f6cfdc35c349d9f7b15663f0c153e1b15
izabellllia <izabellllia@users.noreply.github.com>
2023-09-30 11:35:39 +0000

    Deploying to gh-pages from @ izabellllia/OOP@8a8c6acba447ab380d2f5a269f89d03df4d5cd00 🚀

--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"zhitnik","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"zhitnik","l":"Main"},{"p":"zhitnik","l":"Polinom"}]
