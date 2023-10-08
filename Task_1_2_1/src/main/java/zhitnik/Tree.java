a085f3ae1faa6704bfb14d3b233dd8419924ec27
izabellllia <elizavetazhitnik@MacBook-Air-Elizaveta-3.local>
2023-10-08 20:24:01 +0700

    test5

--- Task_1_2_1/src/main/java/zhitnik/Tree.java
+++ Task_1_2_1/src/main/java/zhitnik/Tree.java
@@ -2,29 +2,46 @@ package zhitnik;
 
 import java.util.*;
 
+/**Класс Tree представляет дерево и имеет корневой узел. Он предоставляет
+ * методы для проверки, пустое ли дерево, и для получения высоты дерева.*/
 class Tree<T> {
+    /**приватное поле, представляющее корневой узел дерева.*/
     private Node<T> root;
 
+    /**конструктор класса, cоздает новый объект Node<T> с
+     * переданными данными и устанавливает его как корень дерева.*/
     public Tree(T data) {
         this.root = new Node<>(data);
     }
 
+    /**метод, возвращающий корневой узел дерева.*/
     public Node<T> getRoot() {
         return root;
     }
 
+    /**метод, устанавливающий переданный узел как корень дерева.*/
     public void setRoot(Node<T> root) {
         this.root = root;
     }
 
+    /**метод, проверяющий, пустое ли дерево (корневой узел равен null).
+     * Возвращает true, если дерево пустое, иначе false.*/
     public boolean isEmpty() {
         return root == null;
     }
 
-    public int getHeight() {
+    /**метод, возвращающий высоту дерева. Высота определяется как максимальное количество уровней
+     * в дереве. Возвращает результат вызова приватного метода getHeight(root).*/
+     public int getHeight() {
         return getHeight(root);
     }
 
+    /**приватный рекурсивный метод, вычисляющий высоту дерева,
+     * начиная с переданного узла. Если узел равен null, возвращает 0.
+     * Иначе, инициализирует переменную height значением 0 и для каждого ребенка
+     * узла вызывает рекурсивно метод getHeight, сравнивая возвращаемое значение с текущим
+     * значением height и присваивая максимальное значение. Возвращает height + 1,
+     * чтобы учесть текущий уровень узла.*/
     private int getHeight(Node<T> node) {
         if (node == null) {
             return 0;
@@ -36,37 +53,71 @@ class Tree<T> {
             return height + 1;
         }
     }
+
+    /**сравнивает текущее дерево с переданным объектом. Если объекты равны по ссылке, то они равны.
+     * Если переданный объект является null или принадлежит другому классу, то они не равны.
+     * Затем метод сравнивает корневые узлы двух деревьев с помощью метода Objects.equals,
+     * который сравнивает объекты на равенство, учитывая возможность null значений.*/
+     @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Tree<?> other = (Tree<?>) obj;
+        return Objects.equals(root, other.root);
+    }
+
 }
 
-class Node<T> {
+/**Представляет узел в дереве. Каждый узел имеет данные типа T и список дочерних узлов. Он предоставляет
+ * методы для получения и установки данных и дочерних узлов, а также для добавления дочернего узла.*/
+ class Node<T> {
+    /**приватное поле, хранящее данные узла.*/
     private T data;
+    /**приватное поле, хранящее список детей текущего узла.*/
     private List<Node<T>> children;
-
-    public Node(T data) {
+    /**конструктор класса, принимающий данные для создания узла.
+     * Создает новый объект Node<T> с переданными данными и
+     * инициализирует пустой список дочерних узлов.*/
+     public Node(T data) {
         this.data = data;
         this.children = new ArrayList<>();
     }
 
-    public T getData() {
+    /**метод, возвращающий данные узла.*/
+     public T getData() {
         return data;
     }
 
-    public void setData(T data) {
+    /** метод, устанавливающий переданные данные как данные узла.*/
+     public void setData(T data) {
         this.data = data;
     }
 
-    public List<Node<T>> getChildren() {
+    /**метод, возвращающий список дочерних узлов текущего узла.*/
+     public List<Node<T>> getChildren() {
         return children;
     }
 
+    /**метод, устанавливающий переданный список дочерних узлов как список дочерних узлов текущего узла.*/
     public void setChildren(List<Node<T>> children) {
         this.children = children;
     }
 
-    public void addChild(Node<T> child) {
+    /**метод, добавляющий переданный узел в список дочерних узлов текущего узла.*/
+     public void addChild(Node<T> child) {
         children.add(child);
     }
 
+    /**сравнивает текущий узел с переданным объектом.
+     * Если объекты равны по ссылке, возвращает true. Если переданный объект равен null
+     * или не является экземпляром класса Node, возвращает false. Если переданный объект
+     * является экземпляром класса Node, приводит его к типу и сравнивает данные и
+     * списки дочерних узлов текущего узла с данными и списками дочерних узлов переданного узла,
+     * используя методы equals класса Objects. Возвращает результат сравнения.*/
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -80,28 +131,41 @@ class Node<T> {
     }
 }
 
+/**Реализует интерфейс Iterable, позволяя его использовать в цикле foreach.
+ * Он принимает корневой узел в конструкторе и предоставляет итератор,
+ * который выполняет обход дерева в ширину. Итератор использует очередь для отслеживания узлов,
+ * которые нужно посетить следующими.*/
 class BreadthFirstIterator<T> implements Iterable<Node<T>> {
-    private Node<T> root;
-
+    /**приватное поле, хранящее корневой узел дерева.*/
+     private Node<T> root;
+    /*конструктор класса, принимающий корневой узел дерева. Инициализирует поле root переданным узлом.*/
     public BreadthFirstIterator(Node<T> root) {
         this.root = root;
     }
 
-    @Override
+    /**переопределенный метод iterator интерфейса Iterable. Создает и возвращает новый объект Iterator<Node<T>>,
+     * который будет использоваться для итерации по дереву.*/
+     @Override
     public Iterator<Node<T>> iterator() {
         return new Iterator<Node<T>>() {
-            private Queue<Node<T>> queue = new LinkedList<>();
+            /**приватное поле, хранящее очередь узлов для обхода в ширину.*/
+             private Queue<Node<T>> queue = new LinkedList<>();
 
             {
                 queue.add(root);
             }
 
-            @Override
+            /**переопределенный метод hasNext интерфейса Iterator. Возвращает true, если очередь не пуста
+             * (есть еще узлы для обхода), и false в противном случае.*/
+             @Override
             public boolean hasNext() {
                 return !queue.isEmpty();
             }
-
-            @Override
+            /**переопределенный метод next интерфейса Iterator. Возвращает следующий узел для обхода в ширину.
+             * Если очередь пуста, генерируется исключение NoSuchElementException.
+             * Иначе, извлекается первый узел из очереди, и для каждого дочернего узла текущего узла добавляется в
+             * очередь. Возвращается извлеченный узел.*/
+             @Override
             public Node<T> next() {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
@@ -115,5 +179,3 @@ class BreadthFirstIterator<T> implements Iterable<Node<T>> {
         };
     }
 }
-
-
--- Task_1_2_1/src/test/java/zhitnik/TreeTest.java
+++ Task_1_2_1/src/test/java/zhitnik/TreeTest.java
@@ -3,11 +3,11 @@ package zhitnik;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**Test.*/
 public class TreeTest {
 
     @Test
     public void testIsEmpty() {
-        // Create a tree with strings
         Tree<String> tree = new Tree<>("A");
         Node<String> root = tree.getRoot();
 
@@ -71,5 +71,72 @@ public class TreeTest {
         }
 
         Assertions.assertEquals("A B C D E F ", output.toString());
+    }
+
+    @Test
+    public void testIsEmptyOnEmptyTree() {
+        // Create an empty tree
+        Tree<Integer> emptyTree = new Tree<>(null);
+        Assertions.assertFalse(emptyTree.isEmpty());
+    }
+
+    @Test
+    void testEquals() {
+        Tree<String> tree1 = new Tree<>("A");
+        Node<String> nodeB1 = new Node<>("B");
+        Node<String> nodeC1 = new Node<>("C");
+        Node<String> nodeD1 = new Node<>("D");
+        Node<String> nodeE1 = new Node<>("E");
+        Node<String> nodeF1 = new Node<>("F");
+
+        tree1.getRoot().addChild(nodeB1);
+        tree1.getRoot().addChild(nodeC1);
+        tree1.getRoot().addChild(nodeD1);
+        tree1.getRoot().addChild(nodeE1);
+        tree1.getRoot().addChild(nodeF1);
+
+        Tree<String> tree2 = new Tree<>("A");
+        Node<String> nodeB2 = new Node<>("B");
+        Node<String> nodeC2 = new Node<>("C");
+        Node<String> nodeD2 = new Node<>("D");
+        Node<String> nodeE2 = new Node<>("E");
+        Node<String> nodeF2 = new Node<>("F");
+
+        tree2.getRoot().addChild(nodeB2);
+        tree2.getRoot().addChild(nodeC2);
+        tree2.getRoot().addChild(nodeD2);
+        tree2.getRoot().addChild(nodeE2);
+        tree2.getRoot().addChild(nodeF2);
+
+        Assertions.assertTrue(tree1.equals(tree2));
+    }
+
+    @Test
+    void testNotEquals() {
+        Tree<String> tree1 = new Tree<>("A");
+        Node<String> nodeB1 = new Node<>("B");
+        Node<String> nodeC1 = new Node<>("C");
+        Node<String> nodeD1 = new Node<>("D");
+        Node<String> nodeE1 = new Node<>("E");
+        Node<String> nodeF1 = new Node<>("F");
+
+        tree1.getRoot().addChild(nodeB1);
+        tree1.getRoot().addChild(nodeC1);
+        tree1.getRoot().addChild(nodeD1);
+        tree1.getRoot().addChild(nodeE1);
+        tree1.getRoot().addChild(nodeF1);
+
+        Tree<String> tree2 = new Tree<>("A");
+        Node<String> nodeB2 = new Node<>("B");
+        Node<String> nodeC2 = new Node<>("C");
+        Node<String> nodeD2 = new Node<>("D");
+        Node<String> nodeE2 = new Node<>("E");
+
+        tree2.getRoot().addChild(nodeB2);
+        tree2.getRoot().addChild(nodeC2);
+        tree2.getRoot().addChild(nodeD2);
+        tree2.getRoot().addChild(nodeE2);
+
+        Assertions.assertFalse(tree1.equals(tree2));
     }
 }
