11c72a64745f5b161cbfc5760a64178fcc8c8b77
izabellllia <elizavetazhitnik@MacBook-Air-Elizaveta-3.local>
2023-10-18 21:40:41 +0700

    fixed mistakes5 test4

--- Task_1_2_1/src/test/java/zhitnik/TreeTest.java
+++ Task_1_2_1/src/test/java/zhitnik/TreeTest.java
@@ -115,27 +115,27 @@ public class TreeTest {
     }
 
     @Test
-    public void testBreadthFirstTraversal() {
-        Tree<String> tree = new Tree<>("A");
-        Node<String> root = tree.getRoot();
-        Node<String> nodeB = new Node<>("B");
-        Node<String> nodeC = new Node<>("C");
-        Node<String> nodeD = new Node<>("D");
-        final Node<String> nodeE = new Node<>("E");
-        final Node<String> nodeF = new Node<>("F");
+    public void testBreadthFirstTraversalWithMultipleLevels() {
+        Tree<Integer> tree = new Tree<>(1);
+        Node<Integer> root = tree.getRoot();
+        Node<Integer> node2 = new Node<>(2);
+        Node<Integer> node3 = new Node<>(3);
+        Node<Integer> node4 = new Node<>(4);
+        final Node<Integer> node5 = new Node<>(5);
+        final Node<Integer> node6 = new Node<>(6);
 
-        root.addChild(nodeB);
-        root.addChild(nodeC);
-        nodeB.addChild(nodeD);
-        nodeB.addChild(nodeE);
-        nodeC.addChild(nodeF);
+        root.addChild(node2);
+        root.addChild(node3);
+        node2.addChild(node4);
+        node2.addChild(node5);
+        node3.addChild(node6);
 
         StringBuilder output = new StringBuilder();
-        for (Node<String> node : new TreeIterableBfs<>(root)) {
+        for (Node<Integer> node : new TreeIterableBfs<>(root)) {
             output.append(node.getData()).append(" ");
         }
 
-        Assertions.assertEquals("A B C D E F ", output.toString());
+        Assertions.assertEquals("1 2 3 4 5 6 ", output.toString());
     }
 
     @Test
