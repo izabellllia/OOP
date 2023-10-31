bcdf5e7765232b3979f4e9e1691bcd098f3f4e9b
izabellllia <elizavetazhitnik@MacBook-Air-Elizaveta-3.local>
2023-10-31 10:13:58 +0700

    draf5 javadoc and coco

--- Task_1_2_2/src/test/java/zhitnik/GraphTest.java
+++ Task_1_2_2/src/test/java/zhitnik/GraphTest.java
@@ -1,46 +1,45 @@
 package zhitnik;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-
-public class GraphTest{
+/**some tests.*/
+public class GraphTest {
     @Test
     public void testSortVerticesByDistance() {
         Graph<String> graph = new Graph<>();
 
-        Vertex<String> A = new Vertex<>("A");
-        Vertex<String> B = new Vertex<>("B");
-        Vertex<String> C = new Vertex<>("C");
-        Vertex<String> D = new Vertex<>("D");
-        Vertex<String> E = new Vertex<>("E");
+        Vertex<String> a = new Vertex<>("A");
+        Vertex<String> b = new Vertex<>("B");
+        Vertex<String> c = new Vertex<>("C");
+        Vertex<String> d = new Vertex<>("D");
+        Vertex<String> e = new Vertex<>("E");
 
-        graph.addVertex(A);
-        graph.addVertex(B);
-        graph.addVertex(C);
-        graph.addVertex(D);
-        graph.addVertex(E);
+        graph.addVertex(a);
+        graph.addVertex(b);
+        graph.addVertex(c);
+        graph.addVertex(d);
+        graph.addVertex(e);
 
-        graph.addEdge(A, B, 2);
-        graph.addEdge(A, C, 4);
-        graph.addEdge(B, C, 1);
-        graph.addEdge(B, D, 4);
-        graph.addEdge(C, D, 3);
-        graph.addEdge(D, E, 2);
+        graph.addEdge(a, b, 2);
+        graph.addEdge(a, c, 4);
+        graph.addEdge(b, c, 1);
+        graph.addEdge(b, d, 4);
+        graph.addEdge(c, d, 3);
+        graph.addEdge(d, e, 2);
 
         List<Vertex<String>> sortedVertices = graph.sortVerticesByDistance(A);
 
-        Assertions.assertEquals(A, sortedVertices.get(0));
-        Assertions.assertEquals(B, sortedVertices.get(1));
-        Assertions.assertEquals(C, sortedVertices.get(2));
-        Assertions.assertEquals(D, sortedVertices.get(3));
-        Assertions.assertEquals(E, sortedVertices.get(4));
+        Assertions.assertEquals(a, sortedVertices.get(0));
+        Assertions.assertEquals(b, sortedVertices.get(1));
+        Assertions.assertEquals(c, sortedVertices.get(2));
+        Assertions.assertEquals(d, sortedVertices.get(3));
+        Assertions.assertEquals(e, sortedVertices.get(4));
     }
     
     @Test
-    public void testAddVertexAMG() {
+    public void testAddVertexAmG() {
         AdjacencyMatrixGraph<String> graph = new AdjacencyMatrixGraph<>(4);
 
         Vertex<String> vertexA = new Vertex<>("A");
@@ -59,7 +58,7 @@ public class GraphTest{
     }
 
     @Test
-    public void testRemoveVertexAMG() {
+    public void testRemoveVertexAmG() {
         AdjacencyMatrixGraph<String> graph = new AdjacencyMatrixGraph<>(4);
 
         Vertex<String> vertexA = new Vertex<>("A");
@@ -80,7 +79,7 @@ public class GraphTest{
     }
 
     @Test
-    public void testAddEdgeAMG() {
+    public void testAddEdgeAmG() {
         AdjacencyMatrixGraph<String> graph = new AdjacencyMatrixGraph<>(4);
 
         Vertex<String> vertexA = new Vertex<>("A");
@@ -99,7 +98,7 @@ public class GraphTest{
     }
 
     @Test
-    public void testRemoveEdgeAMG() {
+    public void testRemoveEdgeAmG() {
         AdjacencyMatrixGraph<String> graph = new AdjacencyMatrixGraph<>(4);
 
         Vertex<String> vertexA = new Vertex<>("A");
@@ -120,7 +119,7 @@ public class GraphTest{
     }
 
     @Test
-    public void testGetVerticesAMG() {
+    public void testGetVerticesAmG() {
         AdjacencyMatrixGraph<String> graph = new AdjacencyMatrixGraph<>(4);
 
         Vertex<String> vertexA = new Vertex<>("A");
