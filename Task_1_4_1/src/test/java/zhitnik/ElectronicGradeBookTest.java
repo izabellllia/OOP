303350de4ba64d116884f68969adae7b93480511
izabellllia <elizavetazhitnik@mail.ru>
2023-11-26 10:52:10 +0700

    fixed import for rdogs3.0((

--- Task_1_4_1/src/test/java/zhitnik/ElectronicGradeBookTest.java
+++ Task_1_4_1/src/test/java/zhitnik/ElectronicGradeBookTest.java
@@ -1,8 +1,8 @@
 package zhitnik;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
