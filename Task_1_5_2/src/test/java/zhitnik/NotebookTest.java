2e34efa54ee442c8df80a03bbd652a2f1fcec6a6
izabellllia <elizavetazhitnik@mail.ru>
2023-12-21 00:37:44 +0700

    add some tests

--- Task_1_5_2/src/test/java/zhitnik/NotebookTest.java
+++ Task_1_5_2/src/test/java/zhitnik/NotebookTest.java
@@ -1,7 +1,9 @@
 package zhitnik;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**super-cool-tests.*/
@@ -31,5 +33,40 @@ public class NotebookTest {
         notebook.addNote("Testovaya Nota 1", "SuperskiyContent 1");
         notebook.addNote("Testovaya Nota 2", "SuperskiyContent 2");
         notebook.serializeToJson("test.json");
+    }
+
+    private Notebook notebook;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @BeforeEach
+    void setUp() {
+        notebook = new Notebook();
+        notebook.addNote("Заголовок 1", "Содержание 1");
+        notebook.addNote("Заголовок 2", "Содержание 2");
+        startTime = LocalDateTime.of(2022, 1, 1, 0, 0);
+        endTime = LocalDateTime.of(2022, 12, 31, 23, 59);
+    }
+
+    @Test
+    void addNote() {
+        Assertions.assertEquals(2, notebook.getAllNotes().size());
+        notebook.addNote("Новая заметка", "Новое содержание");
+        Assertions.assertEquals(3, notebook.getAllNotes().size());
+    }
+
+    @Test
+    void removeNote() {
+        Assertions.assertEquals(2, notebook.getAllNotes().size());
+        notebook.removeNote("Заголовок 1");
+        Assertions.assertEquals(1, notebook.getAllNotes().size());
+    }
+
+    @Test
+    void getAllNotes() {
+        List<Note> notes = notebook.getAllNotes();
+        Assertions.assertEquals(2, notes.size());
+        Assertions.assertEquals("Заголовок 1", notes.get(0).getTitle());
+        Assertions.assertEquals("Содержание 2", notes.get(1).getContent());
     }
 }
